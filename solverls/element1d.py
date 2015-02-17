96af21a982ad3fe16be69256f28139b946cccf47
Alfredo Carella <alfredocarella@gmail.com>
2015-02-17 17:09:15 +0100

    Refactored class Element1D

--- solverls/element1d.py
+++ solverls/element1d.py
@@ -4,50 +4,35 @@ import numpy
 
 class Element1D():
     """Spectral (high order) one-dimensional element. One of the elementary blocks that compose a mesh (and a mesh object)"""
-    #TODO: A suitable docstring should be written here
+    def __init__(self, element_boundaries, local_gm, variables):
 
-    def __init__(self, macro_nodes, gm_cell, list_of_variables):
-        # self.quadrature_weights = []  # element attribute
-        # self.quadrature_points = []  # element attribute
-        # self.jac = []  # element attribute
-        # self.dx = []  # element attribute
-        # self.x = numpy.zeros(self.dof)  # element attribute
-        # self.long_quadrature_weights = []  # element attribute
-        # self.pos = []  # element attribute
-        # for el_ in range(self.number_of_elements):
-        #     lower_element_boundary = self.macro_nodes[el_]
-        #     upper_element_boundary = self.macro_nodes[el_ + 1]
-        #     self.jac.append((upper_element_boundary - lower_element_boundary) / 2.0)
-        #     self.dx.append(lagrange_derivative_matrix_gll(self.element_orders[el_] + 1) / self.jac[el_])
-        #     qx, qw = gll(self.element_orders[el_] + 1, lower_element_boundary, upper_element_boundary)
-        #     self.quadrature_points.append(qx)  # x coordinate (quad points)
-        #     self.quadrature_weights.append(qw)  # quadrature weights
-        #     long_qw = numpy.tile(qw, self.number_of_variables)
-        #     self.long_quadrature_weights.append(long_qw)
-        #     self.pos.append({})
-        #     for var_ in range(self.number_of_variables):
-        #         first_node_in_element = var_ * len(self.gm[el_]) // self.number_of_variables
-        #         last_node_in_element = first_node_in_element + len(self.gm[el_]) // self.number_of_variables
-        #         self.pos[el_][self.list_of_variables[var_]] = numpy.arange(first_node_in_element, last_node_in_element)
-        #     self.x[self.gm[el_]] = numpy.tile(qx, self.number_of_variables)
+        self.variables = variables
+        self.order = (len(local_gm) // len(variables)) - 1
+        self.pos = self.create_local_variable_indices(self.variables, self.order)
 
-        self.number_of_variables = len(list_of_variables)
-        self.element_order = (len(gm_cell) // self.number_of_variables) - 1
-    # for el_ in range(self.number_of_elements):
-        lower_element_boundary = macro_nodes[0]
-        upper_element_boundary = macro_nodes[1]
-        self.jac = (upper_element_boundary - lower_element_boundary) / 2.0
-        self.dx = lagrange_derivative_matrix_gll(self.element_order + 1) / self.jac
-        qx, qw = gll(self.element_order + 1, lower_element_boundary, upper_element_boundary)
-        long_qw = numpy.tile(qw, self.number_of_variables)
-        self.quadrature_points = qx  # x coordinate (quad points)
-        self.quadrature_weights = qw  # quadrature weights
-        self.long_quadrature_weights = long_qw
-        self.pos = {}
-        self.x[gm_cell] = numpy.tile(qx, self.number_of_variables)
-        for var_ in range(self.number_of_variables):
-            first_node_in_variable = var_ * (self.element_order + 1)
-            last_node_in_variable = first_node_in_variable + (self.element_order + 1)
-            self.pos[list_of_variables[var_]] = numpy.arange(first_node_in_variable, last_node_in_variable)
+        self.boundaries = element_boundaries  # geometry
+        self.quadrature_points, self.quadrature_weights = gll(self.order + 1, self.boundaries[0], self.boundaries[1])
+        self.long_quadrature_weights = numpy.tile(self.quadrature_weights, len(variables))
+        self.x = numpy.tile(self.quadrature_points, len(variables))
 
+        self.jac = (self.boundaries[1] - self.boundaries[0]) / 2.0  # Derivatives
+        self.dx = lagrange_derivative_matrix_gll(self.order + 1) / self.jac
+
+    def create_local_variable_indices(self, variables, order):
+        pos = {}
+        for var_ in range(len(variables)):
+            first_node_in_variable = var_ * (order + 1)
+            last_node_in_variable = first_node_in_variable + (order + 1)
+            pos[variables[var_]] = numpy.arange(first_node_in_variable, last_node_in_variable)
+        return pos
+
+
+if __name__ == '__main__':
+    macro_nodes, gm_element, list_of_variables = [1, 2], numpy.arange(4, dtype=numpy.int), ['f1', 'f2']
+    Element1D(macro_nodes, gm_element, list_of_variables)
+
+    print('Test inputs:')
+    print('macro_nodes = %s' % str(macro_nodes))
+    print('gm_element = %s' % str(gm_element))
+    print('list_of_variables = %s' % str(list_of_variables))
 
