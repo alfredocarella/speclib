13a4d6fb5ec452e577391946a55a0a30d3da1337
Alfredo Carella <alfredocarella@gmail.com>
2015-02-24 11:43:46 +0100

    Implemented tests for class Element1D

--- tests/element1d_tests.py
+++ tests/element1d_tests.py
@@ -6,33 +6,19 @@ from solverls.mesh1d import Element1D
 __author__ = 'Alfredo Carella'
 
 
-# def test_element1d():    # Testing the mesh generation (plotting is not tested here)
-#     macro_grid, orders = numpy.array((0.0, 1.0, 2.0, 3.0)), numpy.array((3, 4, 2))
-#     list_of_variables = ['T', 'pres', 'quality']
-#
-#     my_mesh1d = Mesh1D(macro_grid, orders, list_of_variables)
-#
-#     numpy.testing.assert_array_equal(my_mesh1d.element_orders, orders)
-#     numpy.testing.assert_allclose(my_mesh1d.macro_grid, macro_grid)
-#     assert_equal(my_mesh1d.variables, list_of_variables)
-#     assert_equal(len(my_mesh1d.elem), len(my_mesh1d.element_orders))
-#     assert_equal(my_mesh1d.dof, len(my_mesh1d.variables) * (sum(my_mesh1d.element_orders)+1))
-#     assert_equal(len(my_mesh1d.gm), len(my_mesh1d.elem))
-#
-#     integral_test_value = 0
-#     for el in range(len(my_mesh1d.elem)):
-#         integral_test_value += my_mesh1d.elem[el].x_1v.dot(my_mesh1d.elem[el].w_1v)
-#         assert_almost_equal(
-#             my_mesh1d.elem[el].jac, (my_mesh1d.elem[el].x_1v[-1]-my_mesh1d.elem[el].x_1v[0]) / 2.0)
-#         numpy.testing.assert_array_equal(my_mesh1d.gm[el][:my_mesh1d.element_orders[el]+1], my_mesh1d.gm[el][0:(my_mesh1d.elem[el].order + 1)])
-#         numpy.testing.assert_array_equal(my_mesh1d.elem[el].x_1v, my_mesh1d.elem[el].x_1v)
-#         numpy.testing.assert_array_equal(
-#             my_mesh1d.elem[el].dx, speclib.lagrange_derivative_matrix_gll(my_mesh1d.elem[el].order+1)/my_mesh1d.elem[el].jac)
-#         numpy.testing.assert_array_equal(
-#             my_mesh1d.elem[el].w_nv,
-#             numpy.tile(my_mesh1d.elem[el].w_1v, len(my_mesh1d.variables)))
-#         pos_var_test_value = []
-#         for var in my_mesh1d.variables:
-#             pos_var_test_value = numpy.append(pos_var_test_value, my_mesh1d.elem[el].pos[var])
-#         numpy.testing.assert_array_equal(pos_var_test_value, range(len(my_mesh1d.gm[el])))
-#     assert_almost_equal(integral_test_value, (my_mesh1d.macro_grid[-1]-my_mesh1d.macro_grid[0])**2 / 2)
+def test_element1d():
+    my_element = Element1D(boundaries=[2, 3], order=3, variables=['var_1', 'var_2'])
+    assert_equal(Element1D.get_num_instances(), 1)
+    # Test numbering
+    assert_equal(my_element.variables, ['var_1', 'var_2'])
+    assert_equal(my_element.order, 3)
+    numpy.testing.assert_array_equal(my_element.pos['var_2'], [4, 5, 6, 7])
+    # Test geometry
+    assert_equal(my_element.boundaries['x'], [2.0, 3.0])
+    numpy.testing.assert_array_equal(my_element.x_1v, speclib.gll(4, 2, 3)[0])
+    numpy.testing.assert_array_equal(my_element.w_1v, speclib.gll(4, 2, 3)[1])
+    numpy.testing.assert_array_equal(my_element.x_nv, numpy.tile(speclib.gll(4, 2, 3)[0], 2))
+    numpy.testing.assert_array_equal(my_element.w_nv, numpy.tile(speclib.gll(4, 2, 3)[1], 2))
+    # Test differentiation
+    assert_equal(my_element.jac, 1/2.0)
+    numpy.testing.assert_array_equal(my_element.dx, speclib.lagrange_derivative_matrix_gll(4) * 2)
