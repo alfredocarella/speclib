db2951359eeec99f8c016a3928ba5d0baa97436d
Alfredo Carella <alfredocarella@gmail.com>
2015-05-31 00:46:57 +0200

    Encapsulate spectral library tests into a class derived from unittest.TestCase. Replace repeated nested loops by a generator function

--- tests/spectral_tests.py
+++ tests/spectral_tests.py
@@ -7,26 +7,37 @@ from solverls import spectral
 __author__ = 'Alfredo Carella'
 
 
-def test_gll():
-    for number_of_points in range(2, 7):
-        for (x_min, x_max) in [(-1, 1), (2, 5), (-6, -4), (-3, 1)]:
+class TestSpectralLibrary(unittest.TestCase):
+
+    def setUp(self):
+        self.list_of_orders = list(range(2, 7))
+        self.list_of_element_boundaries = [(-1, 1), (2, 5), (-6, -4), (-3, 1)]
+
+    def tearDown(self):
+        del self.list_of_orders
+        del self.list_of_element_boundaries
+
+    def number_of_points_and_boundary_coordinates(self):
+        for order in self.list_of_orders:
+            for (x_min, x_max) in self.list_of_element_boundaries:
+                yield order + 1, (x_min, x_max)
+
+    def test_gll(self):
+        for number_of_points, (x_min, x_max) in self.number_of_points_and_boundary_coordinates():
             points, weights = spectral.gll(number_of_points, x_min, x_max)
-            assert_equal((points[0], points[-1]), (x_min, x_max))
+            self.assertEqual(points[0], x_min)
+            self.assertEqual(points[-1], x_max)
             assert_almost_equal(numpy.sum(weights), x_max - x_min)  # int(1)^{x_max}_{x_min}
             assert_almost_equal(weights.dot(points), (x_max**2 - x_min**2)/2.0)  # int(x)^{x_max}_{x_min}
 
-
-def test_lagrange_derivative_matrix_gll():
-    for number_of_points in range(2, 7):
-        for (x_min, x_max) in [(-1, 1), (2, 5), (-6, -4), (-3, 1)]:
+    def test_lagrange_derivative_matrix_gll(self):
+        for number_of_points, (x_min, x_max) in self.number_of_points_and_boundary_coordinates():
             points, weights = spectral.gll(number_of_points, x_min, x_max)
             dx = spectral.lagrange_derivative_matrix_gll(number_of_points) * 2.0 / (x_max - x_min)
             numpy.testing.assert_allclose(dx.dot(points), numpy.ones(number_of_points))  # d/dx(x)
 
-
-def test_lagrange_interpolating_matrix():
-    for number_of_points in range(2, 7):
-        for (x_min, x_max) in [(-1, 1), (2, 5), (-6, -4), (-3, 1)]:
+    def test_lagrange_interpolating_matrix(self):
+        for number_of_points, (x_min, x_max) in self.number_of_points_and_boundary_coordinates():
             points, weights = spectral.gll(number_of_points, x_min, x_max)
             for plot_resolution in [7, 12, 23]:
                 plot_points = numpy.linspace(x_min, x_max, plot_resolution)
