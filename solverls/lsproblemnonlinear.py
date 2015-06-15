894628a073c09edf70384b0c9050b599d5d9e695
Alfredo Carella <alfredocarella@gmail.com>
2015-06-15 12:12:53 +0200

    Delete unnecessary class "Iterator".

--- solverls/lsproblemnonlinear.py
+++ solverls/lsproblemnonlinear.py
@@ -1,7 +1,7 @@
 import numpy
 
-from solverls.iterator import Iterator
 from solverls.lsproblem import LSProblem
+from solverls.spectral import conj_grad_elem
 
 
 __author__ = 'Alfredo Carella'
@@ -9,16 +9,40 @@ __author__ = 'Alfredo Carella'
 
 class LSProblemNonLinear(LSProblem):
 
-    def solve(self):
-        self.f = numpy.ones(self.mesh.dof)    # seed for guessing the solution
-        it = Iterator(min_residual=1e-20, max_nonlinear_it=50, min_delta=1e-16)
-        it.iterate(self)
+    def solve(self, min_residual=1e-20, max_iter=50, min_delta=1e-16, seed=None):
+        min_residual = min_residual
+        max_iter = max_iter
+        min_delta = min_delta
+
+        delta = 1.0
+        solution_iterations = 0
+        has_converged = False
+        is_converging = True
+        reached_max_iter = False
+
+        self.f = seed if seed else numpy.ones(self.mesh.dof)   # seed for guessing the solution
+        while (not has_converged) and (not reached_max_iter) and is_converging:
+            self.set_operators(el for el in self.mesh.elem)
+            self.set_boundary_conditions()
+            self.f_old = self.f.copy()
+            self.f, num_cg_it = conj_grad_elem(self.k_el, self.g_el, self.mesh.gm)
+            self.residual = sum(self.compute_residual(el) for el in self.mesh.elem)
+            solution_iterations += 1
+            delta = numpy.linalg.norm(self.f - self.f_old) / numpy.linalg.norm(self.f)
+
+            has_converged = self.residual < min_residual
+            is_converging = delta > min_delta
+            reached_max_iter = solution_iterations >= max_iter
+            if not is_converging:
+                raise RuntimeWarning("Equal consecutive nonlinear iterations. Delta = %r" % delta)
+            elif reached_max_iter:
+                raise RuntimeWarning("Reached max nonlinear iterations (%r)." % solution_iterations)
 
         solver_report = "Iterations: {0!r:s}  -  Residual: {1:04.2e}  -  delta = {2:04.2e}"
-        print(solver_report.format(it.solution_iterations, self.residual, it.delta))
+        print(solver_report.format(solution_iterations, self.residual, delta))
 
-    def solve_nonlinear_slab(self):
-        raise NotImplementedError("This method has not been implemented yet.")  # TODO: Pending functionality
+    # def solve_nonlinear_slab(self):
+    #     raise NotImplementedError("This method has not been implemented yet.")  # TODO: Pending functionality
 
 
 if __name__ == '__main__':
